25e1303bdb7ed77b43c0815b78185b75de120d35
BeardedRoar <joeljoel224@hotmail.com>
2015-02-25 11:29:14 +0100

    spellcheck

--- IMat/IMatModel.java
+++ IMat/IMatModel.java
@@ -115,7 +115,7 @@ public class IMatModel {
          return this.handler.isFavorite(p);
      }
      
-     public boolean changeFaoriteStatus(Product p){
+     public boolean changeFavoriteStatus(Product p){
          boolean becomeFavorite = !isFavorite(p);
          if(becomeFavorite)
              handler.addFavorite(p);
