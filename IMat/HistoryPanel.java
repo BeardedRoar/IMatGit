fe3237d869a82798466f8949708a4d11c4122863
Marcus Aurén <auren@student.chalmers.se>
2015-03-04 15:42:57 +0100

    Added header for history

--- IMat/HistoryPanel.java
+++ IMat/HistoryPanel.java
@@ -44,6 +44,10 @@ public class HistoryPanel extends javax.swing.JPanel {
 
         historyScrollPanel = new javax.swing.JScrollPane();
         historyPanel = new javax.swing.JPanel();
+        jPanel1 = new javax.swing.JPanel();
+        dateLabel = new javax.swing.JLabel();
+        orderNumberLabel = new javax.swing.JLabel();
+        totalPriceLabel = new javax.swing.JLabel();
 
         setMinimumSize(new java.awt.Dimension(1000, 600));
         setOpaque(false);
@@ -65,27 +69,72 @@ public class HistoryPanel extends javax.swing.JPanel {
 
         historyScrollPanel.setViewportView(historyPanel);
 
+        jPanel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+
+        dateLabel.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        dateLabel.setText("Date:");
+
+        orderNumberLabel.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        orderNumberLabel.setText("ID:");
+
+        totalPriceLabel.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        totalPriceLabel.setText("Pris:");
+        totalPriceLabel.setMaximumSize(new java.awt.Dimension(134, 29));
+        totalPriceLabel.setMinimumSize(new java.awt.Dimension(134, 29));
+        totalPriceLabel.setPreferredSize(new java.awt.Dimension(134, 29));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(dateLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(266, 266, 266)
+                .addComponent(orderNumberLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(92, 92, 92)
+                .addComponent(totalPriceLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(420, 420, 420))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(dateLabel)
+                    .addComponent(orderNumberLabel)
+                    .addComponent(totalPriceLabel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(historyScrollPanel)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(historyScrollPanel)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(historyScrollPanel)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(historyScrollPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 523, Short.MAX_VALUE)
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel dateLabel;
     private javax.swing.JPanel historyPanel;
     private javax.swing.JScrollPane historyScrollPanel;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel orderNumberLabel;
+    private javax.swing.JLabel totalPriceLabel;
     // End of variables declaration//GEN-END:variables
 }
