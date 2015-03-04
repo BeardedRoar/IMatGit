b5f0c65a666523af0350130577a77e2167b62967
Marcus Aurén <auren@student.chalmers.se>
2015-03-04 15:11:01 +0100

    Made shoppingCartPanel better looking

--- IMat/ConfirmPanel.java
+++ IMat/ConfirmPanel.java
@@ -28,7 +28,7 @@ public class ConfirmPanel extends javax.swing.JPanel {
         this.setBackground(Constants.BACKGROUND_COLOR);
         
         List<ShoppingItem> items = model.getItems();
-        this.shoppingCartPanel.setLayout(new GridLayout(items.size(), 1));
+        this.shoppingCartPanel.setLayout(new GridLayout(Math.max(9,items.size()), 1));
         Iterator<ShoppingItem> it = items.iterator();
         while (it.hasNext()){
             this.shoppingCartPanel.add(new SmallShoppingCartComponentPanel(it.next()));
@@ -410,11 +410,11 @@ public class ConfirmPanel extends javax.swing.JPanel {
         shoppingCartPanel.setLayout(shoppingCartPanelLayout);
         shoppingCartPanelLayout.setHorizontalGroup(
             shoppingCartPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 561, Short.MAX_VALUE)
+            .addGap(0, 580, Short.MAX_VALUE)
         );
         shoppingCartPanelLayout.setVerticalGroup(
             shoppingCartPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 600, Short.MAX_VALUE)
+            .addGap(0, 606, Short.MAX_VALUE)
         );
 
         shoppingCartScrollPane.setViewportView(shoppingCartPanel);
@@ -466,7 +466,7 @@ public class ConfirmPanel extends javax.swing.JPanel {
             containerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(cardCustomerPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(containerPanelLayout.createSequentialGroup()
-                .addComponent(shoppingCartScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, 600, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(shoppingCartScrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 608, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
